e509600169d82d479b23c9ca6f73bd49b85fa2e7
18tj0smh01 <kangminjun990730@gmail.com>
2024-12-20 12:23:46 +0900

    test

--- src/main/java/com/example/board/demo/domain/PostVO.java
+++ src/main/java/com/example/board/demo/domain/PostVO.java
@@ -1,6 +1,7 @@
 package com.example.board.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,13 +9,30 @@ import lombok.Setter;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostVO {
+    @JsonProperty("id")
     private long id;
+
+    @JsonProperty("postTitle")
     private String postTitle;
+
+    @JsonProperty("postContent")
     private String postContent;
+
+    @JsonProperty("postDate")
     private String postDate;
+
+    @JsonProperty("postUpdateTime")
     private String postUpdateTime;
+
+    @JsonProperty("memberId")
     private long memberId;
+
+    @JsonProperty("memberName")
     private String memberName;
+
+    @JsonProperty("viewCount")
     private long viewCount;
+
+    @JsonProperty("commentCount")
     private long commentCount;
 }
